2d58afdb030201e9b49820a1004face6e60e9904
Misty Becker <mistyb01@uw.edu>
2022-08-27 11:54:17 -0700

    design updates

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const allLinks = 
 [
-    {name: "gmail", url: "https://mail.google.com", category: "general"},
-    {name: "notion", url: "https://notion.com", category: "general"},
-    {name: "youtube", url: "https://youtube.com", category: "general"},
-
+    {name: "notion", url: "https://www.notion.so/Web-Dev-09fe194545d0443994a9f3f409c9e2c4", category: "code"},
     {name: "github", url: "https://github.com", category: "code"},
-    {name: "figma", url: "https://figma.com", category: "code"},
     {name: "js30", url: "https://javascript30.com", category: "code"},
 
-    {name: "pinterest", url: "https://pinterest.com", category: "art"},
+    {name: "figma", url: "https://figma.com", category: "design"},
+    {name: "design principles", url: "https://principles.design", category: "design"},
+    {name: "hack design", url: "https://hackdesign.org/lessons", category: "design"},
+    {name: "behance", url: "https://behance.com", category: "design"},
+    
+    {name: "notion", url: "https://www.notion.so/Art-d4180dcd7ba54c498ee4dc7699974d03", category: "art"},
+    {name: "gumroad", url: "https://gumroad.com/library", category: "art"},
+    {name: "pinterest", url: "https://www.pinterest.com/marinlily11/", category: "art"},
+    {name: "adorkastock", url: "https://adorkastock.com", category: "art"},
     {name: "pixiv", url: "https://pixiv.com", category: "art"},
-    {name: "adorkastock", url: "https://adorkastock.com", category: "art"}
+    {name: "tegaki", url: "http://te2.tewi.us/newpost", category: "art"},
+
+    {name: "genki", url: "https://archive.org/details/Genki/Genki%20-%20Elementary%20Japanese%20I/page/n34/mode/2up", category: "jp"},
+
+    {name: "HUNTERxHUNTER", url: "https://mangadex.org/title/db692d58-4b13-4174-ae8c-30c515c0689c/hunter-x-hunter?order=asc", category: "fun"},
 ]
 
-generateLinks('general');
+generateLinks('code');
 
 /*
 TIME: Should be updated in real time.
@@ -59,6 +67,25 @@ function generateLinks(selectedCategory) {
     }
 }
 
+/* night mode */
+var currentTime = new Date()
+let isNight = false;
+if (currentTime.getHours >= 17) {
+    isNight = true;
+}
+const nightBtn = document.getElementById('nightBtn');
+nightBtn.addEventListener('click', handleNightMode);
 
-
+function handleNightMode() {
+    if (isNight === false) {
+        nightBtn.innerText = "☼";
+        document.documentElement.style.setProperty(`--txt`, '#C996CC');
+        document.documentElement.style.setProperty(`--bg`, '#1C0C5B');
+    } else if (isNight === true) {
+        nightBtn.innerText = "☾";
+        document.documentElement.style.setProperty(`--txt`, '#130e5e');
+        document.documentElement.style.setProperty(`--bg`, '#f2e7b9');
+    }
+    isNight = !isNight;
+}
 
